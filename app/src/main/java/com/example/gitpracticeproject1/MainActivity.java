785aa44ecb88c12fc25ebc72d4d727239e9cbb5c
Sooraj Kumar Sharma <soorajsharma7777@gmail.com>
2023-01-16 12:59:59 +0530

    chenging MainActivity

--- app/src/main/java/com/example/gitpracticeproject1/MainActivity.java
+++ app/src/main/java/com/example/gitpracticeproject1/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView.findViewById(R.id.tv);
+        //textView.findViewById(R.id.tv);
         this.sum();
     }
 
