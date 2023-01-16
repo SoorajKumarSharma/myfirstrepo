126e999cb81f07c53e72aff19326f83169ee8481
Sooraj Kumar Sharma <soorajsharma7777@gmail.com>
2023-01-16 12:08:05 +0530

    adding Data_File

---
+++ app/src/main/java/com/example/gitpracticeproject1/Data_File.java
@@ -0,0 +1,14 @@
+package com.example.gitpracticeproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Data_File extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_file);
+    }
+}
--- app/src/main/java/com/example/gitpracticeproject1/MainActivity.java
+++ app/src/main/java/com/example/gitpracticeproject1/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView.findViewById(R.id.tv);
         this.sum();
     }
 
