538d187b81d1d450421d137be1b4590b1d2e3f34
Sooraj Kumar Sharma <soorajsharma7777@gmail.com>
2023-01-16 11:53:55 +0530

    my initial Commit

---
+++ app/src/main/java/com/example/gitpracticeproject1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.gitpracticeproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.sum();
+    }
+
+    public void sum(){
+        int n1 = 225, n2 = 115, sum;
+        sum = n1 + n2;
+        Log.d("Sooraj","The sum of numbers is: "+sum);
+        Toast.makeText(MainActivity.this, "This is sum number is: "+sum, Toast.LENGTH_SHORT).show();
+    }
+}
