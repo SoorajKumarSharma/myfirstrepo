d3848624642c28c3a2653732d96bcbf2b5acb992
SoorajKumarSharma <122784467+SoorajKumarSharma@users.noreply.github.com>
2023-01-17 14:51:11 +0530

    Update

--- app/src/main/java/com/example/gitpracticeproject1/Data_File.java
+++ app/src/main/java/com/example/gitpracticeproject1/Data_File.java
@@ -7,12 +7,24 @@ import android.widget.Toast;
 
 public class Data_File extends AppCompatActivity {
 
+      Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_file);
         
         Toast.makeText(Data_File.this, "This is Data_file class", Toast.LENGTH_SHORT).show();
+        
+        button=findViewById(R.id.butn);
 
+         button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Data_File.this, "Switch on MainActivity", Toast.LENGTH_SHORT).show();
+               
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);   
+            }
+        });
     }
 }
